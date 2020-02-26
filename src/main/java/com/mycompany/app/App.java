69b6fd6fcca9c0269e0a61e9754a51eb2c0f3671
anishsethumadhav <44522196+anishsethumadhav@users.noreply.github.com>
2020-02-26 18:14:22 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World AWS!";
+    private final String message = "Hello World First AWS!";
 
     public App() {}
 
